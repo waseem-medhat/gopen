e4b2b2309827f1e0fca34023224f364c91a54f67
wipdev-tech <wipdev.tech@gmail.com>
2023-10-13 23:41:08 +0300

    initial code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wipdev-tech/gopen
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Gopen - a simple CLI to jump to coding projects")
+}
