a25b171fa19177d5197be1901d9110b1662d8f4c
wipdev-tech <wipdev.tech@gmail.com>
2023-10-17 17:45:54 +0300

    feat: check if config exists

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
-    "os"
-	"os/exec"
+	"fmt"
+	"log"
+	"os"
+	// "os/exec"
 )
 
 func main() {
-    cmd := exec.Command("vi") // or absolute binary path
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    err := cmd.Run()
-    if err != nil {
-        panic(err)
-    }
+	if len(os.Args) < 2 {
+		fmt.Println("Gopen - a simple CLI to quick-start coding projects")
+		return
+	}
+
+	switch os.Args[1] {
+	case "config":
+		checkConfig()
+	}
+	// cmd := exec.Command("vi") // or absolute binary path
+	// cmd.Stdin = os.Stdin
+	// cmd.Stdout = os.Stdout
+	// cmd.Stderr = os.Stderr
+	// err = cmd.Run()
+	// if err != nil {
+	//     panic(err)
+	// }
+}
+
+func checkConfig() {
+	configDir := os.Getenv("HOME") + "/.config/gopen"
+	configPath := configDir + "/gopen.json"
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		fmt.Println("Config file not found - creating one... ")
+
+		err := os.MkdirAll(configDir, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = os.Create(configPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Created an empty config file!")
+	}
+
+	fmt.Println("Config file found!")
 }
