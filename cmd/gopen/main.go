2f3626949ef3fc119dd2c7fe60d1f3aef3a1fd27
wipdev-tech <wipdev.tech@gmail.com>
2023-10-31 19:57:41 +0200

    Abstracted func main logic into handlers
    This is to allow func main to provide a high level overview, and then
    the relevant function can be navigated to on demand.

--- cmd/gopen/main.go
+++ cmd/gopen/main.go
@@ -15,59 +15,79 @@ var configPath = configDir + "/gopen.json"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Gopen - a simple CLI to quick-start coding projects")
+		handleHelp()
 		return
 	}
 
 	switch os.Args[1] {
 	case "--init", "-i":
-		err := config.Init(configDir, configPath)
-		if err != nil {
-			fmt.Println(fmt.Errorf("error: %v", err))
-		}
+		handleInit()
 
 	case "--editor-cmd", "-e":
-		configObj, err := config.Read(configPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(os.Args) < 3 {
-			fmt.Println(configObj.EditorCmd)
-		} else {
-			configObj.EditorCmd = os.Args[2]
-			err := config.Write(configObj, configPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+		handleEditorCmd()
 
 	case "--alias", "-a":
-		configObj, err := config.Read(configPath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(os.Args) < 3 {
-			for _, fmtAlias := range diralias.List(configObj) {
-				fmt.Println(fmtAlias)
-			}
-		} else if len(os.Args) > 4 {
-			fmt.Println("Too many arguments - exiting...")
-		} else {
-			configObj = diralias.Add(configObj, os.Args[2], os.Args[3])
-			err := config.Write(configObj, configPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+		handleAlias()
 
 	default:
-		configObj, err := config.Read(configPath)
+		handleGopen()
+	}
+}
+
+func handleHelp() {
+	fmt.Println("Gopen - a simple CLI to quick-start coding projects")
+}
+
+func handleInit() {
+	err := config.Init(configDir, configPath)
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: %v", err))
+	}
+}
+
+func handleEditorCmd() {
+	configObj, err := config.Read(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 3 {
+		fmt.Println(configObj.EditorCmd)
+	} else {
+		configObj.EditorCmd = os.Args[2]
+		err := config.Write(configObj, configPath)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		gopen.Gopen(os.Args[1], configObj)
 	}
+}
+
+func handleAlias() {
+	configObj, err := config.Read(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 3 {
+		for _, fmtAlias := range diralias.List(configObj) {
+			fmt.Println(fmtAlias)
+		}
+	} else if len(os.Args) > 4 {
+		fmt.Println("Too many arguments - exiting...")
+	} else {
+		configObj = diralias.Add(configObj, os.Args[2], os.Args[3])
+		err := config.Write(configObj, configPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func handleGopen() {
+	configObj, err := config.Read(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gopen.Gopen(os.Args[1], configObj)
 }
