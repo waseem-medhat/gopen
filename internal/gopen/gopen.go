d310ea6d5a04856894e4660f372ee6cd504b42cb
Daniel Hjartland <squashical@gmail.com>
2023-11-07 15:29:02 +0100

    fix formatting

--- internal/gopen/gopen.go
+++ internal/gopen/gopen.go
@@ -2,24 +2,24 @@
 package gopen
 
 import (
-	"errors"
-	"os"
-	"os/exec"
+    "errors"
+    "os"
+    "os/exec"
 
-	"github.com/wipdev-tech/gopen/internal/structs"
+    "github.com/wipdev-tech/gopen/internal/structs"
 )
 
 // Gopen uses the Config struct to find the path corresponding to targetAlias
 // and executes the editor command with the target path as the working
 // directory
 func Gopen(targetAlias string, config structs.Config) (err error) {
-	var targetPath string
-	for _, dirAlias := range config.DirAliases {
-		if targetAlias == dirAlias.Alias {
-			targetPath = dirAlias.Path
-			break
-		}
-	}
+    var targetPath string
+    for _, dirAlias := range config.DirAliases {
+        if targetAlias == dirAlias.Alias {
+            targetPath = dirAlias.Path
+            break
+        }
+    }
 
     if targetPath == "" {
         return errors.New("Invalid command or non-existent alias\nRun `gopen help` for info")
@@ -38,14 +38,14 @@ func Gopen(targetAlias string, config structs.Config) (err error) {
         cmd = exec.Command(editorCmd)
     }
 
-cmd.Stdin = os.Stdin
-cmd.Stdout = os.Stdout
-cmd.Stderr = os.Stderr
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-err = cmd.Run()
-if err != nil {
+    err = cmd.Run()
+    if err != nil {
+        return
+    }
+
     return
-}
-
-return
 }
